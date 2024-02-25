4b1b1a643aec6423f2d28ef0a322fa6fe88c2a43
lajarre <alexandre.hajjar@gmail.com>
2024-02-25 15:24:25 -0500

    ✨ Add kudos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # pylint: disable=unused-argument
+# pylint: disable=logging-fstring-interpolation
 """
 Aligner Telegram Bot
 """
@@ -7,6 +8,7 @@ import datetime
 import logging
 import os
 
+import pytz
 from pyairtable import Api
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
@@ -15,9 +17,12 @@ from telegram.ext import (
     CallbackQueryHandler,
     CommandHandler,
     ContextTypes,
+    ConversationHandler,
     MessageHandler,
     filters,
 )
+
+SAVE_KUDO = 0
 
 TG_BOT_TOK = os.environ["TG_BOT_TOK"]
 TG_BOT_UNAME = os.environ["TG_BOT_UNAME"]
@@ -41,6 +46,12 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 logger = logging.getLogger(__name__)
+
+
+def get_today() -> str:
+    mountain_time = pytz.timezone("America/Denver")
+    now_in_mountain_time = datetime.datetime.now(mountain_time)
+    return now_in_mountain_time.strftime("%Y-%m-%d")
 
 
 async def greet_new_users(update: Update, context: CallbackContext):
@@ -105,7 +116,7 @@ async def join(update: Update, context: ContextTypes.DEFAULT_TYPE):
     except Exception as e:  # pylint: disable=broad-exception-caught
         logger.exception(e)
         await context.bot.send_message(
-            chat_id=user.id, text="An unknown error occured, we're on it."
+            chat_id=user.id, text="🤦 An unknown error occured, we're on it."
         )
     else:
         if existing:
@@ -118,6 +129,130 @@ async def join(update: Update, context: ContextTypes.DEFAULT_TYPE):
             )
 
 
+async def start_kudo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.info(f"kudo {update.effective_user.name}")
+    user = update.effective_user
+    part_table = airtable_api.table(AIRTABLE_BASE_ID, AIRTABLE_DB_PART_ID)
+    kudo_table = airtable_api.table(AIRTABLE_BASE_ID, AIRTABLE_DB_VOTE_ID)
+
+    try:
+        existing_part = part_table.all(formula=f"{{Telegram ID}}={user.id}")
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        logger.exception(e)
+        await context.bot.send_message(
+            chat_id=user.id, text="🤦 An unknown error occured, we're on it."
+        )
+        return ConversationHandler.END
+
+    if not existing_part:
+        await context.bot.send_message(
+            chat_id=user.id,
+            text=(
+                "🤌 You need to join first. Join first by hitting Join "
+                "or sending the /join command."
+            ),
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Join", callback_data="/join")]]
+            ),
+        )
+        return ConversationHandler.END
+
+    part_id = existing_part[0]["fields"]["ID"]
+    today = get_today()
+
+    try:
+        existing_kudos = kudo_table.all(formula=f"FIND({part_id},{{Participant}})")
+        existing_kudos_today = [
+            k for k in existing_kudos if k["fields"]["Date"] == today
+        ]
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        logger.exception(e)
+        await context.bot.send_message(
+            chat_id=user.id, text="🤦 An unknown error occured, we're on it."
+        )
+        return ConversationHandler.END
+
+    if len(existing_kudos_today) >= 3:
+        names = [k["fields"]["Kudoee Telegram handle"] for k in existing_kudos_today]
+        # [TODO] Add a reset procedure but this needs to be tied to a date.
+        await context.bot.send_message(
+            chat_id=user.id,
+            text=(
+                f"💗 You already gave your 3 kudos today, to {', '.join(names)}.\n"
+                "🙏 Please get in touch with the team to reset your Kudos for today."
+            ),
+        )
+        return ConversationHandler.END
+
+    await context.bot.send_message(
+        chat_id=user.id,
+        text=(
+            "Please write the Telegram handle of the person you'd like to send a kudo to."
+            "\nIf you don't want to send a kudo anymore, just send /cancel."
+        ),
+    )
+    context.user_data["part_id"] = part_id
+    context.user_data["day"] = today
+    return SAVE_KUDO
+
+
+async def unsafe_save_kudo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    name = update.message.text
+    user = update.effective_user
+    kudo_table = airtable_api.table(AIRTABLE_BASE_ID, AIRTABLE_DB_VOTE_ID)
+
+    part_id = context.user_data["part_id"]
+    del context.user_data["part_id"]
+    day = context.user_data["day"]
+    del context.user_data["day"]
+
+    fields = {
+        "Participant": [part_id],
+        "Kudoee Telegram handle": name,
+        "Date": day,
+    }
+
+    try:
+        kudo_table.create(fields)
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        logger.exception(e)
+        await context.bot.send_message(
+            chat_id=user.id, text="🤦 An unknown error occured, we're on it."
+        )
+        return ConversationHandler.END
+
+    await context.bot.send_message(
+        chat_id=user.id,
+        text=(f"💌 Thank you for sending your appreciation to {name}!"),
+    )
+
+    return ConversationHandler.END
+
+
+async def cancel_kudo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    await context.bot.send_message(chat_id=user.id, text="Kudo operation canceled.")
+    return ConversationHandler.END
+
+
+async def catch_all(update: Update, context: CallbackContext):
+    user = update.effective_user
+
+    try:
+        await context.bot.send_message(
+            chat_id=user.id,
+            text=(
+                "🤷 Command not understood. If you haven't joined yet "
+                "and want to join as an Alignooor, send /join."
+            ),
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Join", callback_data="/join")]]
+            ),
+        )
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        logger.exception(e)
+
+
 async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.info(f"join {update.effective_user.name}")
     query = update.callback_query
@@ -126,6 +261,8 @@ async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await start(update, context)
     if query.data == "/join":
         await join(update, context)
+    if query.data == "/kudo":
+        await join(start_kudo, context)
 
 
 def main() -> None:
@@ -134,12 +271,27 @@ def main() -> None:
 
     tg_app.add_handler(CommandHandler("start", start))
     tg_app.add_handler(CommandHandler("join", join))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("kudo", start_kudo)],
+        states={
+            SAVE_KUDO: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, unsafe_save_kudo)
+            ]
+        },
+        fallbacks=[
+            CommandHandler("cancel", cancel_kudo),
+            MessageHandler(filters.COMMAND, lambda update, context: None),
+        ],
+    )
+    tg_app.add_handler(conv_handler)
 
     tg_app.add_handler(CallbackQueryHandler(button_callback))
 
     tg_app.add_handler(
         MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, greet_new_users)
     )
+
+    tg_app.add_handler(MessageHandler(filters.TEXT | filters.COMMAND, catch_all))
 
     # Run the bot until the user presses Ctrl-C
     tg_app.run_polling(allowed_updates=Update.ALL_TYPES)
